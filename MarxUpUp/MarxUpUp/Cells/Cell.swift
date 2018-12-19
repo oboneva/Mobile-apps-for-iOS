3ed02773297b5bed25c3e71b20a90dbb64783329
oboneva <ogi.boneva@gmail.com>
2018-12-19 09:47:38 +0200

    adds missing space

--- MarxUpUp/MarxUpUp/Cells/Cell.swift
+++ MarxUpUp/MarxUpUp/Cells/Cell.swift
@@ -17,7 +17,7 @@ extension UITableViewCell {
 }
 
 extension UITableView {
-    func dequeueReusableCell<T: UITableViewCell>(fromClass cellClass: T.Type, forIndexPath indexPath: IndexPath) ->T {
+    func dequeueReusableCell<T: UITableViewCell>(fromClass cellClass: T.Type, forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withIdentifier: cellClass.identifier, for: indexPath) as? T else {
             print("Cell from class" + "\(cellClass)" + "cannot be dequeued")
             return UITableViewCell() as! T
@@ -34,7 +34,7 @@ extension UICollectionViewCell {
 }
 
 extension UICollectionView {
-    func dequeueReusableCell<T: UICollectionViewCell>(fromClass cellClass: T.Type, forIndexPath indexPath: IndexPath) ->T {
+    func dequeueReusableCell<T: UICollectionViewCell>(fromClass cellClass: T.Type, forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withReuseIdentifier: cellClass.identifier, for: indexPath) as? T else {
             print("Cell from class" + "\(cellClass)" + "cannot be dequeued")
             return UICollectionViewCell() as! T
