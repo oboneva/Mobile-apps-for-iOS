3664f9f292965c00d0ede0bed80ed2b11b97cc27
oboneva <ogi.boneva@gmail.com>
2018-12-19 09:22:58 +0200

    moves some of the logic to new methods

--- MarxUpUp/MarxUpUp/Annotations/Utilities.swift
+++ MarxUpUp/MarxUpUp/Annotations/Utilities.swift
@@ -10,20 +10,19 @@ import UIKit
 
 class Utilities: NSObject {
 
-    class func rectBetween(_ firstPoint: CGPoint, _ secondPoint: CGPoint) -> CGRect {
-        let beginPointX = min(firstPoint.x, secondPoint.x)
-        let beginPointY = min(firstPoint.y, secondPoint.y)
-        
-        let endPointX = max(firstPoint.x, secondPoint.x)
-        let endPointY = max(firstPoint.y, secondPoint.y)
-        
-        let width = endPointX - beginPointX
-        let height = endPointY - beginPointY
-        
-        return CGRect(x: beginPointX, y: beginPointY, width: width, height: height)
+    static func rectBetween(_ firstPoint: CGPoint, _ secondPoint: CGPoint) -> CGRect {
+        let points = Utilities.beginAndEndPointForRectBetweenPoints(firstPoint, secondPoint)
+        return CGRect(x: points.0.x, y: points.0.y, width: points.1.x - points.0.x, height: points.1.y - points.0.y)
     }
     
-    class func convert(_ point: CGPoint, fromViewWithSize viewSize: CGSize, andContentInAspectFitModeWithSize contentSize: CGSize) -> CGPoint {
+    private static func beginAndEndPointForRectBetweenPoints(_ firstPoint: CGPoint, _ secondPoint: CGPoint) -> (CGPoint, CGPoint) {
+        let beginPoint = CGPoint(x: min(firstPoint.x, secondPoint.x), y: min(firstPoint.y, secondPoint.y))
+        let endPoint = CGPoint(x: max(firstPoint.x, secondPoint.x),y: max(firstPoint.y, secondPoint.y))
+        
+        return (beginPoint, endPoint)
+    }
+    
+    static func convert(_ point: CGPoint, fromViewWithSize viewSize: CGSize, andContentInAspectFitModeWithSize contentSize: CGSize) -> CGPoint {
         let ratioX = viewSize.width / contentSize.width
         let ratioY = viewSize.height / contentSize.height
         
@@ -33,5 +32,18 @@ class Utilities: NSObject {
         let offsetY = (viewSize.height - contentSize.height * scale) / 2
         
         return CGPoint(x: (point.x - offsetX) / scale, y: (point.y - offsetY) / scale)
+    }
+    
+    static func rotateBezierPath(_ path: UIBezierPath, aroundPoint point: CGPoint, withAngle angle: CGFloat) {
+        path.apply(CGAffineTransform(translationX: -point.x, y: -point.y))
+        path.apply(CGAffineTransform(rotationAngle: angle + .pi / 2))
+        path.apply(CGAffineTransform(translationX: point.x, y:  point.y))
+    }
+    
+    static func angleBetweenPoint(_ point: CGPoint, andOtherPoint other: CGPoint) -> CGFloat {
+        let dx = point.x - other.x
+        let dy = point.y - other.y
+        
+        return CGFloat(atan2(dy, dx))
     }
 }
